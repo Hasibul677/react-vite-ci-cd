003b5b22d2024c928642765f8236a679c08b514b
hasibul.hasan <hasibul.hasan@businessaccelerate.com.bd>
2026-01-07 10:53:46 +0600

    Initial Vite React app

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "60px" }}>
+      <h1>⚡ Vite + React CI/CD</h1>
+      <p>Auto deploy to Netlify 🚀</p>
+    </div>
+  );
+}
+
+export default App;
